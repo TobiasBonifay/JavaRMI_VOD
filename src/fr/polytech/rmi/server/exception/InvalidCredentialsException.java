7e871c618f2ddbb72f1fda17c981b04fbf856fba
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-09-28 15:29:19 +0200

    Exception constructor

--- src/fr/polytech/rmi/server/exception/InvalidCredentialsException.java
+++ src/fr/polytech/rmi/server/exception/InvalidCredentialsException.java
@@ -1,4 +1,10 @@
 package fr.polytech.rmi.server.exception;
 
+/**
+ * Exception thrown when user types a wrong pair name, password
+ */
 public class InvalidCredentialsException extends Exception {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
 }
--- src/fr/polytech/rmi/server/exception/SignInFailedException.java
+++ src/fr/polytech/rmi/server/exception/SignInFailedException.java
@@ -1,4 +1,10 @@
 package fr.polytech.rmi.server.exception;
 
+/**
+ *
+ */
 public class SignInFailedException extends Exception {
+    public SignInFailedException(String message) {
+        super(message);
+    }
 }
