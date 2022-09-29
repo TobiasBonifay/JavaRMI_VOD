6fc9dbd026afacb20c54f9f053578cf3ed49bea1
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-09-29 20:10:03 +0200

    Connection & Client rework
    Simplify
    Scanner should not be closed on method if static method attribut

--- src/fr/polytech/rmi/client/Client.java
+++ src/fr/polytech/rmi/client/Client.java
@@ -12,7 +12,6 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -26,18 +25,17 @@ public class Client implements Serializable {
     private String email;
     private String password;
 
-    public void enterCredentials(){
-        System.out.print("Email : ");
-        this.email = SCANNER.nextLine();
-        System.out.print("Password : ");
-        this.password = SCANNER.nextLine();
-        SCANNER.close();
-    }
-
     public static void main(String[] args) throws NotBoundException, RemoteException {
         Client c1 = new Client();
         c1.runClient();
         LOGGER.info("Finished");
+    }
+
+    private void enterCredentials() {
+        System.out.print("Email : ");
+        this.email = SCANNER.nextLine();
+        System.out.print("Password : ");
+        this.password = SCANNER.nextLine();
     }
 
     private void runClient() throws RemoteException, NotBoundException {
@@ -46,44 +44,31 @@ public class Client implements Serializable {
         final IConnectionService stubConnexion = (IConnectionService) reg.lookup("ConnexionServ");
         LOGGER.info("Establishing connection...");
 
-        System.out.println("Do you have an account ? (y/n)");
-        String choice = SCANNER.nextLine();
-
-        while( ! choice.equals("y") && ! choice.equals("n")){
-            choice = null;
+        String choice;
+        do {
             System.out.println("Do you have an account ? (y/n)");
-            choice = SCANNER.nextLine();
-        }
+            choice = SCANNER.nextLine().trim().toLowerCase();
+        } while (!(choice.equals("y") || choice.equals("n")));
 
-        switch (choice) {
-            case "y" -> {
-                System.out.println("LOG IN :");
-                enterCredentials();
-            }
-            case "n" -> {
-                System.out.println("Create your account :");
-                enterCredentials();
+        System.out.println(choice.equals("y") ? "login" : "register");
+        enterCredentials();
 
-                try {
-                    stubConnexion.signIn(email, password);
-                    LOGGER.info("The account is now signed in");
-                } catch (SignInFailedException e) {
-                    LOGGER.severe("Failed to sign in");
-                    throw new RuntimeException(e);
-                }
+        if (choice.equals("n")) {
+            try {
+                stubConnexion.signIn(email, password);
+            } catch (SignInFailedException e) {
+                LOGGER.severe("Failed to sign in");
+                throw new RuntimeException(e);
             }
         }
-
-
-
-
 
         try {
             final IVODService vodService = stubConnexion.login(email, password);
             try {
                 byte[] data = vodService.flow();
                 LOGGER.info("Data received length : " + data.length);
-                System.out.println(Arrays.toString(data));
+                // System.out.println(Arrays.toString(data));
+                System.out.println(data.length > 300 ? "Works" : "Problem here");
             } catch (IOException e) {
                 LOGGER.severe("vodService can't read the requested content.\n" + e);
             }
