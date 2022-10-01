9d7d9b3a222ffdf03322ee82a999424d05d8211b
TobiasBonifay <tobias.bonifay@etu.univ-cotedazur.fr>
2022-10-01 19:18:58 +0200

    just a rename

--- src/fr/polytech/rmi/CONSTANTS.java
+++ src/fr/polytech/rmi/CONSTANTS.java
@@ -2,7 +2,7 @@ package fr.polytech.rmi;
 
 
 public class CONSTANTS {
-    public static final String CONNEXIONSERV = "ConnexionServ";
+    public static final String CONNEXION_SERV = "ConnexionServ";
     public static final int DEFAULT_PORT = 1099;
     public static final String FILE_DB = "save.db";
 }
